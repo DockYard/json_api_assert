e71b515cc8fe6eb74be8cd2dd69130f667ecf864
Cody Roberts <codyjroberts@gmail.com>
2016-07-30 14:46:41 -0500

    Change exception assertions to match prev style

--- test/jsonapi_test.exs
+++ test/jsonapi_test.exs
@@ -63,31 +63,46 @@ defmodule AssertJsonApiTest do
   end
 
   test "will raise when both errors and data objects exist in the response" do
-    msg = ~r/the members `data` and `errors` MUST NOT coexist in the same document/
+    msg = "the members `data` and `errors` MUST NOT coexist in the same document"
 
-    assert_raise(ExUnit.AssertionError, msg, fn ->
+    try do
       Map.merge(data(:payload), %{"errors" => []})
       |> assert_jsonapi(version: "1.0")
-    end)
+    rescue
+      error in [ExUnit.AssertionError] ->
+        assert msg == error.message
+    else
+      _ -> assert false, message: "Expected Exception #{msg}"
+    end
   end
 
   test "will raise when an included object exists in the response without the presence of the data object" do
-    msg = ~r/If a document does not contain a top-level data key, the included member MUST NOT be present either./
+    msg = "If a document does not contain a top-level data key, the included member MUST NOT be present either."
 
-    assert_raise(ExUnit.AssertionError, msg, fn ->
+    try do
       Map.delete(data(:payload), "data")
       |> assert_jsonapi(version: "1.0")
-    end)
+    rescue
+      error in [ExUnit.AssertionError] ->
+        assert msg == error.message
+    else
+      _ -> assert false, message: "Expected Exception #{msg}"
+    end
   end
 
   test "will raise if document does not contain at least one of the following objects: 'data', 'errors', 'meta'" do
-    msg = ~r/A document MUST contain at least one of the following top-level members: 'data', 'errors', 'meta'/
+    msg = "A document MUST contain at least one of the following top-level members: 'data', 'errors', 'meta'"
 
-    assert_raise(ExUnit.AssertionError, msg, fn ->
+    try do
       Map.delete(data(:payload), "data")
       |> Map.delete("included")
       |> assert_jsonapi(version: "1.0")
-    end)
+    rescue
+      error in [ExUnit.AssertionError] ->
+        assert msg == error.message
+    else
+      _ -> assert false, message: "Expected Exception #{msg}"
+    end
 
     Map.delete(data(:payload), "data")
     |> Map.delete("included")
